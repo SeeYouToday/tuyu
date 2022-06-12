f8785367ea76497c3efdf5349f0ce9c151db3767
pukusyou <pukusho@gmail.com>
2022-06-13 01:30:00 +0900

    改悪に対応しました

--- Tuyu.java
+++ Tuyu.java
@@ -2,6 +2,18 @@
  * このゲームのメインとなるクラス
  */
 public class Tuyu {
-    
+	private ArrayList<Player> player;
+
+    public Tuyu(){
+        player = new ArrayList<>;
+    }
+
+    public AddPlayer(Strategy strategy){
+        player.add(strategy);
+    }
+
+    public static void main(String[] args) {
+        
+    }
     
 }
