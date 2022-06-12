f1b9b1cbb43378dd35596adeba6cb23618c1c777
Muthun0916 <76974949+Muthun0916@users.noreply.github.com>
2022-06-13 01:18:06 +0900

    改悪しますたm9(^Д^)ﾌﾟｷﾞｬｰ

--- BoyStrategy.java
+++ BoyStrategy.java
@@ -1,124 +1,125 @@
-public class BoyStrategy implements Strategy{
-    private Player boy;
-    public BoyStrategy(){
-        boy = new Player(40, 3);
-    }
-    /**
-     * 攻撃カードの選択をする
-     * ただし選択してreturnするのはCardクラスのオブジェクトじゃなくカードが手札の何番目にあるかを返す
-     * 
-     * 選択基準は自身の手札枚数
-     * 最大火力を出すか最小火力を出すの二択
-     * @return 使用カードが手札の何番目にあるか
-     */
-    public int decideAttack(){
-        int ret = 999;
-        Card max = null;
-        Card min = null;
-        Card tmp;
-        boolean flag = false;
-        for(int i = 0; i < boy.getHandMaisu(); i++){
-            if(boy.getHand().check(i).getKind().equals(Card.KIND.ATTACK)){
-                max = boy.getHand().check(i);
-                min = max;
-                flag = true;
-                break;
-            }
-        }
-        if(flag){
-            if(boy.getHandMaisu() < 4){
-                for(int i = 1; i < boy.getHandMaisu(); i++){
-                    tmp = boy.getHand().check(i);
-                    if(max.getNum() < tmp.getNum()){
-                        max = tmp;
-                        ret = i;
-                    }
-                }
-            }
-            else{
-                for(int i = 0; i < boy.getHandMaisu(); i++){
-                    tmp = boy.getHand().check(i);
-                    if(min.getNum() > tmp.getNum()){
-                        min = tmp;
-                        ret = i;
-                    }
-                }
-            }
-        }
+public class BoyStrategy extends Strategy {
 
-        if(ret == 999){
-            boy.getHand().add(boy.getDeck().draw());
-        }
-        return ret;
-    }
-    /**
-     * 防御カードを選択する
-     * 攻撃と同様手札の何番目にあるかを返す
-     * 
-     * 選択基準は自身の体力
-     * 今回相手が選択した攻撃カードの攻撃力より小さい最大防御力または攻撃力より大きい最小防御力
-     * @return 使用カードが手札の何番目にあるか
-     */
-    public int decideDefence(){
-        int ret = 999;
-        Card max = null;
-        Card min = null;
-        Card tmp;
-        boolean flag = false;
-        boolean flag2 = false;
-        for(int i = 0; i < boy.getHandMaisu(); i++){
-            if(boy.getHand().check(i).getKind().equals(Card.KIND.DEFENCE)){
-                max = boy.getHand().check(i);
-                min = max;
-                flag = true;
-                break;
-            }
-        }
-        if(flag){
-            if(boy.getHP() > 10){
-                for(int i = 1; i < boy.getHandMaisu(); i++){
-                    tmp = boy.getHand().check(i);
-                    if(max.getNum() < tmp.getNum() && boy.getPain() >= tmp.getNum()){
-                        max = tmp;
-                        ret = i;
-                        flag2 = true;
-                    }
-                }
-                if(!flag2){
-                    for(int i = 0; i < boy.getHandMaisu(); i++){
-                        tmp = boy.getHand().check(i);
-                        if(min.getNum() > tmp.getNum() && boy.getPain() <= tmp.getNum()){
-                            min = tmp;
-                            ret = i;
-                        }
-                    }
-                }
-            }
-            else{
-                for(int i = 0; i < boy.getHandMaisu(); i++){
-                    tmp = boy.getHand().check(i);
-                    if(min.getNum() > tmp.getNum() && boy.getPain() <= tmp.getNum()){
-                        min = tmp;
-                        ret = i;
-                        flag2 = true;
-                    }
-                }
-                if(!flag2){
-                    for(int i = 1; i < boy.getHandMaisu(); i++){
-                        tmp = boy.getHand().check(i);
-                        if(max.getNum() < tmp.getNum() && boy.getPain() >= tmp.getNum()){
-                            max = tmp;
-                            ret = i;
-                            flag2 = true;
-                        }
-                    }
-                }
-            }
-        }
+	public BoyStrategy() {
+		hp = 40;
+		maisuu = 3;
+	}
 
-        if(ret == 999){
-            boy.getHand().add(boy.getDeck().draw());
-        }
-        return ret;
-    }
+	/**
+	 * 攻撃カードの選択をする
+	 * ただし選択してreturnするのはCardクラスのオブジェクトじゃなくカードが手札の何番目にあるかを返す
+	 *
+	 * 選択基準は自身の手札枚数
+	 * 最大火力を出すか最小火力を出すの二択
+	 * @return 使用カードが手札の何番目にあるか
+	 */
+	public int decideAttack(Player boy) {
+		int ret = 999;
+		Card max = null;
+		Card min = null;
+		Card tmp;
+		boolean flag = false;
+		for (int i = 0; i < boy.getHandMaisu(); i++) {
+			if (boy.getHand().check(i).getKind().equals(Card.KIND.ATTACK)) {
+				max = boy.getHand().check(i);
+				min = max;
+				flag = true;
+				break;
+			}
+		}
+		if (flag) {
+			if (boy.getHandMaisu() < 4) {
+				for (int i = 1; i < boy.getHandMaisu(); i++) {
+					tmp = boy.getHand().check(i);
+					if (max.getNum() < tmp.getNum()) {
+						max = tmp;
+						ret = i;
+					}
+				}
+			} else {
+				for (int i = 0; i < boy.getHandMaisu(); i++) {
+					tmp = boy.getHand().check(i);
+					if (min.getNum() > tmp.getNum()) {
+						min = tmp;
+						ret = i;
+					}
+				}
+			}
+		}
+
+		if (ret == 999) {
+			boy.getHand().add(boy.getDeck().draw());
+		}
+		return ret;
+	}
+
+	/**
+	 * 防御カードを選択する
+	 * 攻撃と同様手札の何番目にあるかを返す
+	 *
+	 * 選択基準は自身の体力
+	 * 今回相手が選択した攻撃カードの攻撃力より小さい最大防御力または攻撃力より大きい最小防御力
+	 * @return 使用カードが手札の何番目にあるか
+	 */
+	public int decideDefence(Player boy) {
+		int ret = 999;
+		Card max = null;
+		Card min = null;
+		Card tmp;
+		boolean flag = false;
+		boolean flag2 = false;
+		for (int i = 0; i < boy.getHandMaisu(); i++) {
+			if (boy.getHand().check(i).getKind().equals(Card.KIND.DEFENCE)) {
+				max = boy.getHand().check(i);
+				min = max;
+				flag = true;
+				break;
+			}
+		}
+		if (flag) {
+			if (boy.getHP() > 10) {
+				for (int i = 1; i < boy.getHandMaisu(); i++) {
+					tmp = boy.getHand().check(i);
+					if (max.getNum() < tmp.getNum() && boy.getPain() >= tmp.getNum()) {
+						max = tmp;
+						ret = i;
+						flag2 = true;
+					}
+				}
+				if (!flag2) {
+					for (int i = 0; i < boy.getHandMaisu(); i++) {
+						tmp = boy.getHand().check(i);
+						if (min.getNum() > tmp.getNum() && boy.getPain() <= tmp.getNum()) {
+							min = tmp;
+							ret = i;
+						}
+					}
+				}
+			} else {
+				for (int i = 0; i < boy.getHandMaisu(); i++) {
+					tmp = boy.getHand().check(i);
+					if (min.getNum() > tmp.getNum() && boy.getPain() <= tmp.getNum()) {
+						min = tmp;
+						ret = i;
+						flag2 = true;
+					}
+				}
+				if (!flag2) {
+					for (int i = 1; i < boy.getHandMaisu(); i++) {
+						tmp = boy.getHand().check(i);
+						if (max.getNum() < tmp.getNum() && boy.getPain() >= tmp.getNum()) {
+							max = tmp;
+							ret = i;
+							flag2 = true;
+						}
+					}
+				}
+			}
+		}
+
+		if (ret == 999) {
+			boy.getHand().add(boy.getDeck().draw());
+		}
+		return ret;
+	}
 }
--- Player.java
+++ Player.java
@@ -2,108 +2,123 @@
  * プレーヤーの内部情報保持クラス
  */
 public class Player {
-    private int hp;
-    private int handMaisu;
-    private boolean isDefence;
-    private Hand hand;
-    private Deck deck;
-    private int pain;
+	private int hp;
+	private int handMaisu;
+	private boolean isDefence;
+	private Hand hand;
+	private Deck deck;
+	private int pain;
+	private Strategy strategy;
 
-    /**
-     * 
-     * @param hp，体力
-     * @param handMaisu，手札数
-     * @param isDefence，前回相手プレイヤーが防御行動をとったか否か，初期はtrue
-     * 
-     */
+	/**
+	 *
+	 * @param hp，体力
+	 * @param handMaisu，手札数
+	 * @param isDefence，前回相手プレイヤーが防御行動をとったか否か，初期はtrue
+	 *
+	 */
 
-    public Player(int hp, int handMaisu){
-        this.hp = hp;
-        this.handMaisu = handMaisu;
-        this.isDefence = true;
-        for(int i = 0; i < handMaisu; i++){
-            hand.add(deck.draw());
-        }
-    }
+	public Player(String strategyName) {
+		this.strategy = setStrategy(strategyName);
+		this.hp = strategy.getterHp();
+		this.handMaisu = strategy.getterMaisuu();
+		this.isDefence = true;
+		/*
+		for (int i = 0; i < handMaisu; i++) {
+			hand.add(deck.draw());
+		}
+		*/
+	}
 
-    /**
-     * hpのセッター
-     * @param hp
-     */
-    public void setHP(int hp){
-        this.hp = hp;
-    }
+	public Strategy setStrategy(String strategyName) {
+		if (strategyName.equals(Strategy.STRATEGY.BOYSTRATEGY.name())) {
+			return new BoyStrategy();
+		} else if (strategyName.equals(Strategy.STRATEGY.GIRLSTRATEGY.name())) {
+			return new GirlStrategy();
+		}
+		System.out.println("いや");
+		return null;
 
-    /**
-     * hpのゲッター
-     * @return 現在の体力
-     * 
-     */
-    public int getHP(){
-        return hp;
-    }
+	}
 
-    /**
-     * handのセッター
-     * @param handMaisu
-     */
-    public void setHandMaisu(int handMaisu){
-        this.handMaisu = handMaisu;
-    }
+	/**
+	 * hpのセッター
+	 * @param hp
+	 */
+	public void setHP(int hp) {
+		this.hp = hp;
+	}
 
-    /**
-     * handのゲッター
-     * @return 現在の手札枚数
-     */
-    public int getHandMaisu(){
-        return handMaisu;
-    }
+	/**
+	 * hpのゲッター
+	 * @return 現在の体力
+	 *
+	 */
+	public int getHP() {
+		return hp;
+	}
 
-    /**
-     * isDefenceのセッター
-     * @param isDefence
-     */
-    public void setIsDefence(boolean isDefence){
-        this.isDefence = isDefence;
-    }
+	/**
+	 * handのセッター
+	 * @param handMaisu
+	 */
+	public void setHandMaisu(int handMaisu) {
+		this.handMaisu = handMaisu;
+	}
 
-    /**
-     * isDefenceのゲッター
-     * @return 前回の防御行動の有無
-     */
-    public boolean getIsDefence(){
-        return isDefence;
-    }
+	/**
+	 * handのゲッター
+	 * @return 現在の手札枚数
+	 */
+	public int getHandMaisu() {
+		return handMaisu;
+	}
 
-    /**
-     * 手札を返すゲッター
-     * @return 手札
-     */
-    public Hand getHand(){
-        return hand;
-    }
+	/**
+	 * isDefenceのセッター
+	 * @param isDefence
+	 */
+	public void setIsDefence(boolean isDefence) {
+		this.isDefence = isDefence;
+	}
 
-    /**
-     * デッキを返すゲッター
-     * @return デッキ
-     */
-    public Deck getDeck(){
-        return deck;
-    }
+	/**
+	 * isDefenceのゲッター
+	 * @return 前回の防御行動の有無
+	 */
+	public boolean getIsDefence() {
+		return isDefence;
+	}
 
-    /**
-     * 相手からのダメージのセッター
-     * @param pain 痛み
-     */
-    public void setPain(int pain){
-        this.pain = pain;
-    }
+	/**
+	 * 手札を返すゲッター
+	 * @return 手札
+	 */
+	public Hand getHand() {
+		return hand;
+	}
 
-    /**
-     * 痛みを返す
-     * @return　相手の攻撃力
-     */
-    public int getPain(){
-        return pain;
-    }
+	/**
+	 * デッキを返すゲッター
+	 * @return デッキ
+	 */
+	public Deck getDeck() {
+		return deck;
+	}
+
+	/**
+	 * 相手からのダメージのセッター
+	 * @param pain 痛み
+	 */
+	public void setPain(int pain) {
+		this.pain = pain;
+	}
+
+	/**
+	 * 痛みを返す
+	 * @return　相手の攻撃力
+	 */
+	public int getPain() {
+		return pain;
+	}
 }
