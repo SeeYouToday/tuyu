ce80d6858eb465bee03912c41491a08e9787e605
selra <ishidai042@gmail.com>
2022-06-14 01:18:14 +0900

    deckの中身入れたおぉぉ

--- Deck.java
+++ Deck.java
@@ -10,16 +10,16 @@ public class Deck {
 
     private Deck(){
     	for(int i = 0; i < 3; i++){
-            decks.add(new Card(10, Card.KIND.DEFENCE, "gandou"));
-            decks.add(new Card(100000, Card.KIND.ATTACK, "uono"));
-            decks.add(new Card(1, Card.KIND.ATTACK, "ちくりんてぉ"));
-            decks.add(new Card(2, Card.KIND.DEFENCE, "カエルくん"));
-            decks.add(new Card(2, Card.KIND.ATTACK, "c"));
-            decks.add(new Card(3, Card.KIND.ATTACK, "d"));
-            decks.add(new Card(4, Card.KIND.ATTACK, "e"));
-            decks.add(new Card(5, Card.KIND.ATTACK, "f"));
-            decks.add(new Card(6, Card.KIND.ATTACK, "g"));
-            decks.add(new Card(7, Card.KIND.ATTACK, "h"));
+            decks.add(new Card(1, Card.KIND.DEFENCE, "がんどう"));
+            decks.add(new Card(6, Card.KIND.ATTACK, "たこ"));
+            decks.add(new Card(2, Card.KIND.ATTACK, "ちくりんてぉ"));
+            decks.add(new Card(8, Card.KIND.ATTACK, "カエルくん"));
+            decks.add(new Card(9, Card.KIND.DEFENCE, "かたつむり"));
+            decks.add(new Card(7, Card.KIND.DEFENCE, "めんつゆ"));
+            decks.add(new Card(3, Card.KIND.ATTACK, "かめくん"));
+            decks.add(new Card(4, Card.KIND.ATTACK, "にんじんソード"));
+            decks.add(new Card(10, Card.KIND.ATTACK, "梅雨前線ソード"));
+            decks.add(new Card(5, Card.KIND.ATTACK, "雨の雰囲気のある葉"));
         }
         shuffle();
     	}
@@ -34,11 +34,11 @@ public class Deck {
     public void shuffle(){
         for(int i = 0; i < decks.size(); i++){
             int shu = rand.nextInt(decks.size());
-            decks.add(decks.remove(shu-1));
+            decks.add(decks.remove(shu));
         }
         for(int i = 0; i < decks.size(); i++){
             int shu = rand.nextInt(decks.size());
-            decks.add(decks.remove(shu-1));
+            decks.add(decks.remove(shu));
         }
     }
 
