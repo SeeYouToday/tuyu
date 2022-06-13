2d297e5976a061115eb436f1db258402342176af
Mutsuttochan <76974949+Muthun0916@users.noreply.github.com>
2022-06-13 18:29:08 +0900

    お天気

--- Testdrive.java
+++ Testdrive.java
@@ -5,6 +5,15 @@ public class Testdrive {
 		Player 女 = new Player(Strategy.STRATEGY.GIRLSTRATEGY.name());
 		System.out.println(男.getHP());
 		System.out.println(女.getHP());
+		
+		Weather.WEATHER weather = Weather.getRandomWeather();
+		System.out.println("天気: "+weather.toString()+"を生成");
+		System.out.println(weather.getDescription());
+		int damage = 300;
+		System.out.println("攻撃力は"+damage);
+		int changed = Weather.calculate(weather, damage);
+		System.out.println("この天気での攻撃力は"+changed);
+		
 
 	}
 
