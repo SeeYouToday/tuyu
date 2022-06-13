9d9a7ca862cf20bfe93ce3a31a35b1f1f89c16f7
pukusyou <pukusho@gmail.com>
2022-06-14 03:06:36 +0900

    コメントアウト完成

--- TuyuAction.java
+++ TuyuAction.java
@@ -1,42 +1,88 @@
-
+/**
+ * ゲームのカード、HPを扱う
+ * @author 北川奨真
+ */
 public class TuyuAction {
+	/*
+	 * attackCard: 攻撃カードのインスタンス
+	 * defenceCard: 防御カードのインスタンス
+//	 * tuyu: 移譲するためのインスタンス
+	 */
 	private Card attackCard, defenceCard;
 	private Tuyu tuyu;
 	
+	/**
+	 * コンストラクタ
+	 * @param attackCard 攻撃カード
+	 * @param defenceCard 防御カード
+	 * @param tuyu 移譲するためのインスタンス
+	 */
 	public TuyuAction(Card attackCard, Card defenceCard, Tuyu tuyu) {
 		this.attackCard = attackCard;
 		this.defenceCard = defenceCard;
 		this.tuyu = tuyu;
 	}
 	
+	/**
+	 * 攻撃カードの攻撃力を返す
+	 * @return 攻撃力
+	 */
 	public int getAttackCardPower() {
 		return attackCard.getNum();
 	}
 	
+	/**
+	 * 防御カードの防御力を返す
+	 * @return 防御力
+	 */
 	public int getDefenceCardPower() {
 		return defenceCard.getNum();
 	}
 	
+	/**
+	 *  攻撃カードの名前を返す
+	 * @return カード名
+	 */
 	public String getAttackCardName() {
 		return attackCard.getName();
 	}
 	
+	/**
+	 * 防御カードの名前を返す
+	 * @return カード名
+	 */
 	public String getDefenceCardName() {
 		return defenceCard.getName();
 	}
 	
+	/**
+	 * 攻撃カードとして選択されたカードの種類を返す
+	 * @return カード種類
+	 */
 	public Card.KIND getAttackCardKind() {
 		return attackCard.getKind();
 	}
 	
+	/**
+	 * 防御カードとして選択されたカードの種類を返す
+	 * @return カード種類
+	 */
 	public Card.KIND getDefenceCardKind() {
 		return defenceCard.getKind();
 	}
 	
+	/**
+	 * メインの処理
+	 * @param DefencePlayerNum 防御側のプレイヤ番号
+	 */
     public void action(int DefencePlayerNum) {
         actionHP(DefencePlayerNum);
     }
     
+    /**
+     * メインの処理(HP処理)
+     * @param DefencePlayerNum 防御側のプレイヤ番号
+     */
     private void actionHP(int DefencePlayerNum) {
     	int hp = tuyu.getPlayer(DefencePlayerNum).getHP();
     	int newHp;
