d35f7a8441060215c1d27890e31c5f878ac42a39
selra <ishidai042@gmail.com>
2022-06-14 15:09:56 +0900

    ゲーム内からカード位置番号

--- PlayerStrategy.java
+++ PlayerStrategy.java
@@ -8,14 +8,36 @@ public class PlayerStrategy extends Strategy {
 
 	@Override
 	public int decideAttack(Player player) {
-		// TODO 自動生成されたメソッド・スタブ
-		return 0;
+		boolean existAttack = false;
+		for(Card card: player.getHand().getHands()){
+			if(card.getKind() == Card.KIND.ATTACK){
+				existAttack = true;
+				break;
+			}
+		}
+		if(existAttack){
+			return 0;//ここの0のところにゲーム内で選択したカードの手札での位置番号を入れたいんや
+		}else{
+			player.getHand().add(Deck.getInstance().draw());
+			return -1;
+		}
 	}
 
 	@Override
 	public int decideDefence(Player player) {
-		// TODO 自動生成されたメソッド・スタブ
-		return 0;
+		boolean existDefence = false;
+		for(Card card: player.getHand().getHands()){
+			if(card.getKind() == Card.KIND.DEFENCE){
+				existDefence = true;
+				break;
+			}
+		}
+		if(existDefence){
+			return 0;//ここの0のところにゲーム内で選択したカードの手札での位置番号を入れたいんや
+		}else{
+			player.getHand().add(Deck.getInstance().draw());
+			return -1;
+		}
 	}
 
 }
