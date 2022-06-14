d59fe8352220191781a6f93f8ab43731b66a901f
pukusyou <pukusho@gmail.com>
2022-06-15 03:40:30 +0900

    エラーコメントアウト

--- Testdrive.java
+++ Testdrive.java
@@ -1,10 +1,10 @@
 public class Testdrive {
 
 	public static void main(String[] args) {
-		Player 男 = new Player(Strategy.STRATEGY.BOYSTRATEGY.name());
-		Player 女 = new Player(Strategy.STRATEGY.GIRLSTRATEGY.name());
-		System.out.println(男.getHP());
-		System.out.println(女.getHP());
+//		Player 男 = new Player(Strategy.STRATEGY.BOYSTRATEGY.name());
+//		Player 女 = new Player(Strategy.STRATEGY.GIRLSTRATEGY.name());
+//		System.out.println(男.getHP());
+//		System.out.println(女.getHP());
 
 	}
 
