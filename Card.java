7b722c45001b5873cf539d4ccf9a4a9f049911d8
pukusyou <pukusho@gmail.com>
2022-06-14 14:58:28 +0900

    GUIのgameメソッドを整形, EnumにtoString設定

--- Card.java
+++ Card.java
@@ -1,52 +1,98 @@
 /**
- * カードを表すクラス
- * Enumもあるよ！！！
+ * カードを表すクラス Enumもあるよ！！！
  */
-public class Card{
+public class Card {
 
-    private int num;
-    KIND kind;
-    private String name;
+	private int num;
+	KIND kind;
+	private String name;
 
-    public enum KIND{
-        ATTACK, DEFENCE, HEAL, TOKEN, END, WIN, LOSE, PEACE;
-        public static KIND[] getKind(){
-            return KIND.values();
-        } 
-    }
+	public enum KIND {
+		ATTACK {
+			@Override
+			public String toString() {
+				return "(攻)";
+			}
+		},
+		DEFENCE {
+			@Override
+			public String toString() {
+				return "(防)";
+			}
+		},
+		HEAL {
+			@Override
+			public String toString() {
 
-    public Card(int num, KIND kind, String name){
-        this.num = num;
-        this.kind = kind;
-        this.name = name;
-    }
-    /**
-     * カードの（攻撃力，防御力などの）値を表すnumを返す
-     * @return カードの数値
-     * 
-     */
-    public int getNum(){
-        return num;
-    }
+				return "(癒)";
+			}
+		},
+		TOKEN, END {
+			@Override
+			public String toString() {
+				return "終了";
+			}
+		},
+		WIN {
+			@Override
+			public String toString() {
+				return "勝利";
+			}
+		},
+		LOSE {
+			@Override
+			public String toString() {
+				return "敗北";
+			}
+		},
+		PEACE {
+			@Override
+			public String toString() {
+				return "引き分け";
+			}
+		};
 
-    /**
-     * カードの種類（攻撃カードなのか防御カードなのか）
-     * @return カードの種類
-     */
-    public KIND getKind(){
-        return kind;
-    }
+		public static KIND[] getKind() {
+			return KIND.values();
+		}
+	}
 
-    /**
-     * カードの名前のゲッター
-     * @return カード名
-     */
-    public String getName(){
-        return name;
-    }
+	public Card(int num, KIND kind, String name) {
+		this.num = num;
+		this.kind = kind;
+		this.name = name;
+	}
 
-    @Override
-    public String toString(){
-        return name;
-    }
+	/**
+	 * カードの（攻撃力，防御力などの）値を表すnumを返す
+	 * 
+	 * @return カードの数値
+	 * 
+	 */
+	public int getNum() {
+		return num;
+	}
+
+	/**
+	 * カードの種類（攻撃カードなのか防御カードなのか）
+	 * 
+	 * @return カードの種類
+	 */
+	public KIND getKind() {
+		return kind;
+	}
+
+	/**
+	 * カードの名前のゲッター
+	 * 
+	 * @return カード名
+	 */
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
 }
